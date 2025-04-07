6719705c5239d34386e69a1271b608619139a266
aadityasah1 <145247265+aadityasah1@users.noreply.github.com>
2025-04-07 11:24:25 +0530

    Update shl_recommender.py

--- shl_recommender.py
+++ shl_recommender.py
@@ -121,5 +121,5 @@ async def recommend(data: QueryInput):
 
 #Run Locally
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # use PORT env var or default to 8000
+    port = int(os.environ.get("PORT", 10000))  # use PORT env var or default to 8000
     uvicorn.run(app, host="0.0.0.0", port=port)
